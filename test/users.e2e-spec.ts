cfad25e05e82c4f3bd45ca822b6c2706db5e343b
Jonghyeon Choi <whd617@naver.com>
2024-05-16 10:20:24 +0900

    test/createAccount Resolve e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,8 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from '../src/app.module';
+
+// got jest.mock 함수 설정하여 실제로 got를 사용하지는 않는다.
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
 import { DataSource } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -17,10 +27,68 @@ describe('UserModule (e2e)', () => {
   });
 
   afterAll(async () => {
-    await DataSource.prototype.dropDatabase();
+    //DataSource.prototype.dropDatabase;
+    const dataSource: DataSource = new DataSource({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_BASE,
+    });
+    const connection: DataSource = await dataSource.initialize();
+    await connection.dropDatabase(); // 데이터베이스 삭제
+    await connection.destroy(); // 연결 해제
     await app.close();
   });
 
+  describe('createAccount', () => {
+    const EMAIL = 'new@account.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation {
+          createAccount(input:{
+            email:"${EMAIL}"
+            password:"1212"
+            role:Owner
+          }){
+            ok
+            error
+          }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    // createAccount User가 이미 존재했을 떄 e2e test
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation {
+          createAccount(input:{
+            email:"${EMAIL}"
+            password:"1212"
+            role:Owner
+          }){
+            ok
+            error
+          }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
