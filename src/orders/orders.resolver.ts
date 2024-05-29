d5ee0533a07f4ad1a2ce3a918f72667fda28bcdc
Jonghyeon Choi <whd617@naver.com>
2024-05-29 13:14:37 +0900

    -chore/PUBSUB global module 작성

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -8,14 +8,16 @@ import { Role } from 'src/auth/role.decorator';
 import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
+import { Inject } from '@nestjs/common';
+import { PUB_SUB } from 'src/common/common.constants';
 import { PubSub } from 'graphql-subscriptions';
-
-// PubSub engin 인스턴스 생성
-const pubsub = new PubSub();
 
 @Resolver((of) => Order)
 export class OrderResolver {
-  constructor(private readonly ordersService: OrderService) {}
+  constructor(
+    private readonly ordersService: OrderService,
+    @Inject(PUB_SUB) private readonly pubSub: PubSub,
+  ) {}
 
   @Mutation((returns) => CreateOrdersOutput)
   @Role(['Client'])
@@ -55,7 +57,7 @@ export class OrderResolver {
 
   @Mutation((returns) => Boolean)
   potatoReady() {
-    pubsub.publish('hotPotatoes', {
+    this.pubSub.publish('hotPotatoes', {
       orederSubscription: 'Your potato is ready. love you',
     });
     return true;
@@ -65,6 +67,6 @@ export class OrderResolver {
   @Role(['Any'])
   orederSubscription(@AuthUser() user: User) {
     console.log(user);
-    return pubsub.asyncIterator('hotPotatoes');
+    return this.pubSub.asyncIterator('hotPotatoes');
   }
 }
